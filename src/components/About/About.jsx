0206dc5ec08a52c5205e79d4650c3d7cc4609371
Shivkumar-7 <kumar00shiv00k123@gmail.com>
2025-08-21 20:03:00 +0530

    update3

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -81,7 +81,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/145YTdbYdjH9vppLEkyIjnJmf3w-3fdrr/view?usp=sharing"
+            href="https://drive.google.com/file/d/1yI58CHoXhOts6HzM2hbN8-Xkwin-hfZ1/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
